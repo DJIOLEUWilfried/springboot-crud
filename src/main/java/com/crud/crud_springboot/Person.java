42e71c4ca3e6738161031750a00870d454ca2c4b
DJIOLEUWilfried <djioleuwilfried@gmail.com>
2025-09-16 13:47:04 +0100

    First commit - springboot-crud

---
+++ src/main/java/com/crud/crud_springboot/Person.java
@@ -0,0 +1,52 @@
+package com.crud.crud_springboot;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "persons")
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String name;
+
+    private String city;
+
+    private String phoneNumber;
+
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+}
---
+++ src/main/java/com/crud/crud_springboot/PersonController.java
@@ -0,0 +1,30 @@
+package com.crud.crud_springboot;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/persons")
+public class PersonController {
+
+    final PersonRepository personRepository;
+
+    public PersonController(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Person>> getAllPerson() {
+        return new ResponseEntity<>(personRepository.findAll(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Person> createPerson(@RequestBody Person person) {
+
+        return new ResponseEntity<>(personRepository.save(person), HttpStatus.CREATED) ;
+    }
+
+}
